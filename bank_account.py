d2e7d02f634fb066ab0d6c22d67214c15e7606ae
Chalisa <Chalisa.ki@ku.th>
2023-11-02 11:20:55 +0700

    Procudural bank account simulation

--- bank_account.py
+++ bank_account.py
@@ -21,6 +21,15 @@ class AccountDB:
                 return account
         return None
 
+    def delete_account(self, account_num):
+        index = self.__search_private(account_num)
+        if index != -1:
+            del self.account_database[index]
+            print(f"Deleting account: {account_num}")
+            # del self.account_database[index]
+        else:
+            print(f"{account_num}invalid account number; nothing to be deleted.")
+
     def __str__(self):
         s = ''
         for account in self.account_database:
@@ -63,5 +72,8 @@ my_account_DB.search_public("0003").deposit(50)
 print(my_account_DB)
 my_account_DB.search_public("0003").withdraw(100)
 print(my_account_DB)
-my_account_DB.search_public("0010").deposit(50)
+# my_account_DB.search_public("0010").deposit(50)
+# print(my_account_DB)
+
+my_account_DB.delete_account('0003')
 print(my_account_DB)
